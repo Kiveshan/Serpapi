4c125d784256b071027cdfe1456034ce6407450f
Ntokozo Ngcobo <49594805+NTOKOZOTK@users.noreply.github.com>
2026-03-26 11:21:36 +0200

    Added the landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,26 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchPage from './components/SearchPage';
 import ResultsPage from './components/ResultsPage';
+import LandingPage from './pages/landingPage/views/landingPage';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <header className="header">
-          <div className="header-content">
-            <h1>
-             Publications
-            </h1>
-          </div>
-        </header>
-        
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<SearchPage />} />
-            <Route path="/results/:authorName" element={<ResultsPage />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/results/:authorName" element={<ResultsPage />} />
+        </Routes>
       </div>
     </Router>
   );
