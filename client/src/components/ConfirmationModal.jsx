13f271297e69e92888f7e1b2be40186a01c6589a
keyan-moodley <keyanmoodley21@gmail.com>
2026-03-27 10:32:15 +0200

    Base

---
+++ client/src/components/ConfirmationModal.jsx
@@ -0,0 +1,8 @@
+import styles from './css/ConfirmationModal.module.css';
+
+const ConfirmationModal = () => {
+    return (
+    );
+};
+
+export default ConfirmationModal;
---
+++ client/src/pages/system-admin/views/ViewApplication.jsx
@@ -0,0 +1,8 @@
+import styles from '../css/ViewApplication.module.css';
+
+const ViewApplication = () => {
+    return (
+    );
+};
+
+export default ViewApplication;
